8c27df95f675d01d9845764b1a50755ebbff45a2
Xavi <xgil78@gmail.com>
2015-07-31 17:49:34 +0200

    ReadFileTask is cancelled when exiting the ListActivity

--- app/src/main/java/com/xavigil/uniquewords/view/ListActivity.java
+++ app/src/main/java/com/xavigil/uniquewords/view/ListActivity.java
@@ -18,6 +18,7 @@ public class ListActivity extends AppCompatActivity {
     public static final String EXTRA_RESOURCE = "com.xavigil.uniquewords.extra_resource";
 
     private RecyclerView mRecyclerView;
+    private ReadFileTask mReadFileTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +30,8 @@ public class ListActivity extends AppCompatActivity {
 
         ProgressBar pb = (ProgressBar)findViewById(R.id.progressBar);
         String resource = getIntent().getExtras().getString(EXTRA_RESOURCE);
-        (new ReadFileTask(this, pb, adapter)).execute(resource);
+        mReadFileTask = new ReadFileTask(this, pb, adapter);
+        mReadFileTask.execute(resource);
     }
 
     @Override
@@ -54,6 +56,12 @@ public class ListActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mReadFileTask.cancel(true);
+    }
+
     private ListAdapter setupRecyclerView(){
         ListAdapter adapter = new ListAdapter();
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -61,18 +69,5 @@ public class ListActivity extends AppCompatActivity {
         return adapter;
     }
 
-//    private List<UniqueWord> getMockList(){
-//        List<UniqueWord> list = new ArrayList<>();
-//        list.add(new UniqueWord("Abracadabra pata de cabra que me voy", 131));
-//        list.add(new UniqueWord("Beacomb", 101));
-//        list.add(new UniqueWord("Conrwall", 98));
-//        list.add(new UniqueWord("Dámaris", 98));
-//        list.add(new UniqueWord("Escorial", 53));
-//        list.add(new UniqueWord("Feo", 12));
-//        list.add(new UniqueWord("Gramática", 8));
-//        list.add(new UniqueWord("Hielo", 8));
-//        list.add(new UniqueWord("Indignados", 3));
-//        list.add(new UniqueWord("Jovellanos", 1));
-//        return list;
-//    }
+
 }
